2ac443a142984797c9bd46d323f92b16e65bf258
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 <Jmandeville@protonmail.com>
2023-11-14 21:02:36 -0500

    url fix

--- Store_DB/app/__init__.py
+++ Store_DB/app/__init__.py
@@ -33,7 +33,7 @@ def create_app():
     app.config.from_object(Config)
 
     db.init_app(app)
-    CORS(app, resources={r"/api/*": {"origins": "https://6554194f84c095385749c127--profound-palmier-bc28df.netlify.app/"}})
+    CORS(app, resources={r"/api/*": {"origins": "https://6554259984c0953ff349c0fe--profound-palmier-bc28df.netlify.app"}})
     login_manager.init_app(app)
     migrate.init_app(app, db)
 
